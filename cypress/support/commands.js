aa890c715771256195f8e414785c62bf61b3c502
viniciuscaine <viniciuscainee@gmail.com>
2023-10-17 09:25:18 -0300

    Create cypress project

---
+++ cypress/support/commands.js
@@ -0,0 +1,34 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (nome, sobrenome, email, texto) => {
+    cy.get('#firstName').type(nome)
+    cy.get('#lastName').type(sobrenome)
+    cy.get('#email').type(email)
+    cy.get('#open-text-area').type(texto)
+    cy.contains('button', 'Enviar').click()
+    cy.get('.success').should('be.visible')
+})
